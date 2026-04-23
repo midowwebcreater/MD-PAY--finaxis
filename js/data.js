24b7633b85caabbe674edbb7a3be8365b007c61a
MIDOWWEBCRETOR <midowwebcreater@gmail.com>
2026-04-23 18:16:17 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 if(!localStorage.getItem("users")){
   localStorage.setItem("users", JSON.stringify({
-    user1: { password:"1234", balance:500, bonus:0, role:"user" },
-    admin1: { password:"admin123", balance:0, bonus:0, role:"admin" }
+    user1:{password:"1234",balance:500,role:"user"},
+    admin1:{password:"admin123",balance:0,role:"admin"}
   }));
 }
